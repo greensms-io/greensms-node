0615f185f3843e2af0136462e76aa68a7aa43998
Manan Jadhav <manan.jadhav@gmail.com>
2020-10-19 09:31:52 +0530

    Added babel and eslint

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,8 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import filesize from "rollup-plugin-filesize";
 import { terser } from "rollup-plugin-terser";
+import { eslint } from "rollup-plugin-eslint";
+import babel from "@rollup/plugin-babel";
 import pkg from "./package.json";
 
 export default {
@@ -10,20 +12,33 @@ export default {
     {
       file: pkg.main,
       format: "cjs",
+      exports: "auto",
       compact: true,
     },
     {
       file: pkg.module,
-      format: "es", // the preferred format
+      format: "esm", // the preferred format
+      exports: "auto",
       compact: true,
     },
     {
       file: pkg.browser,
       format: "iife",
+      exports: "auto",
       name: pkg.moduleName, // the global which can be used in a browser
       compact: true,
     },
   ],
   external: [...Object.keys(pkg.dependencies || {})],
-  plugins: [resolve(), commonjs(), terser(), filesize()],
+  plugins: [
+    resolve(),
+    commonjs(),
+    terser(),
+    filesize(),
+    babel({ babelHelpers: "bundled" }),
+    eslint({
+      fix: true,
+      // throwOnError: true,
+    }),
+  ],
 };
