ca6031c930a33ae70697dbc525fd719e7c13b18d
Manan Jadhav <manan.jadhav@gmail.com>
2020-10-19 18:13:16 +0530

    Added dts plugin for definition

---
+++ index.d.ts
@@ -0,0 +1,20 @@
+enum APIVersion {
+  V1 = "V1",
+}
+
+export interface GreenSMSRequestConfig {
+  username?: string;
+  password?: string;
+  token?: string;
+  useTokenForRequests?: boolean;
+  version?: APIVersion;
+}
+
+export declare class GreenSMS {
+  /**
+   * Creates a GreenSMS Client Object
+   * @constructor
+   * @param GreenSMSOptions
+   */
+  constructor(GreenSMSOptions);
+}
--- rollup.config.js
+++ rollup.config.js
@@ -1,44 +1,58 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import babel from "@rollup/plugin-babel";
 import filesize from "rollup-plugin-filesize";
+import dts from "rollup-plugin-dts";
 import { terser } from "rollup-plugin-terser";
 import { eslint } from "rollup-plugin-eslint";
-import babel from "@rollup/plugin-babel";
+
 import pkg from "./package.json";
 
-export default {
-  input: "lib/index.js",
-  output: [
-    {
-      file: pkg.main,
-      format: "cjs",
-      exports: "auto",
-      compact: true,
-    },
-    {
-      file: pkg.module,
-      format: "esm", // the preferred format
-      exports: "auto",
-      compact: true,
-    },
-    {
-      file: pkg.browser,
-      format: "iife",
-      exports: "auto",
-      name: pkg.moduleName, // the global which can be used in a browser
-      compact: true,
-    },
-  ],
-  external: [...Object.keys(pkg.dependencies || {})],
-  plugins: [
-    resolve(),
-    commonjs(),
-    terser(),
-    filesize(),
-    babel({ babelHelpers: "bundled" }),
-    eslint({
-      fix: true,
-      // throwOnError: true,
-    }),
-  ],
-};
+export default [
+  {
+    input: "./index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
+  {
+    input: "lib/index.js",
+    output: [
+      {
+        file: pkg.main,
+        format: "cjs",
+        exports: "auto",
+        compact: true,
+      },
+      {
+        file: pkg.module,
+        format: "esm", // the preferred format
+        exports: "auto",
+        name: pkg.moduleName,
+        compact: true,
+      },
+      {
+        file: pkg.browser,
+        format: "iife",
+        exports: "auto",
+        name: pkg.moduleName, // the global which can be used in a browser
+        compact: true,
+      },
+    ],
+    external: [...Object.keys(pkg.dependencies || {})],
+    plugins: [
+      resolve(),
+      commonjs(),
+      terser(),
+      filesize(),
+      babel({
+        babelHelpers: "bundled",
+        inputSourceMap: true,
+        sourceMaps: true,
+      }),
+      eslint({
+        fix: true,
+        // throwOnError: true,
+      }),
+    ],
+  },
+];
