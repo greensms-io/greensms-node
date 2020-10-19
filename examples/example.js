41dc8737cc4d841ab7016e73e76c90aeecf01a25
Manan Jadhav <manan.jadhav@gmail.com>
2020-10-19 20:41:14 +0530

    Test examples added

--- examples/example.js
+++ examples/example.js
@@ -1,8 +1,16 @@
 "use strict";
 
-const GreenSMS = require("../dist/index.cjs");
+const GreenSMS = require("../dist/index.cjs.js");
 
-const username = process.env.GREEN_SMS_USERNAME;
-const password = process.env.GREEN_SMS_PASSWORD;
+const username = "test"; // process.env.GREEN_SMS_USERNAME;
+const password = "pass"; // process.env.GREEN_SMS_PASSWORD;
 
 const greenSms = new GreenSMS({ username, password });
+greenSms.sms
+  .sendMessage()
+  .then((data) => {
+    console.log("Data", data);
+  })
+  .catch((err) => {
+    console.error("Error", err);
+  });
