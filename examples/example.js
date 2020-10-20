5e57e16f4ba87ec4f7c350885b8c97c1c35fc078
Manan Jadhav <manan.jadhav@gmail.com>
2020-10-20 18:55:19 +0530

    Added example for send message and message status

--- examples/example.js
+++ examples/example.js
@@ -9,40 +9,55 @@ const password = process.env.GREEN_SMS_PASSWORD;
 
 const greenSms = new GreenSMS({ username, password, camelCaseResponse: true  });
 
-greenSms.account
-  .fetchBalance()
-  .then((data) => {
-    console.log("Balance Data", data);
-  })
-  .catch((err) => {
-    console.error("Balance Error", err);
-  });
-
-
-const tokenParams = {
-  expire: 100, // Optional
-};
-greenSms.account
-  .fetchToken(tokenParams).then((data) => {
-    console.log("Token Data", data);
-  }).catch((err) => {
-    console.error("Token Error", err);
-  });
-
-greenSms.account.fetchTariff().then((data) => {
-  console.error("Tariff Data", data);
-}).catch((err) => {
-  console.error("Tariff Error", err);
-})
-
-// greenSms.sms
-//   .sendMessage()
+// greenSms.account
+//   .fetchBalance()
 //   .then((data) => {
-//     console.log("Data", data);
+//     console.log("Balance Data", data);
 //   })
 //   .catch((err) => {
-//     console.error("Error", err);
+//     console.error("Balance Error", err);
 //   });
+
+
+// const tokenParams = {
+//   expire: 100, // Optional, time in seconds
+// };
+// greenSms.account.fetchToken(tokenParams).then((data) => {
+//   console.log("Token Data", data);
+// }).catch((err) => {
+//   console.error("Token Error", err);
+// });
+
+// greenSms.account.fetchTariff().then((data) => {
+//   console.error("Tariff Data", data);
+// }).catch((err) => {
+//   console.error("Tariff Error", err);
+// });
+
+// greenSms.sms
+//   .sendMessage({
+//     to: '919987409698',
+//     txt: 'Here is your message for delivery',
+//     from: 'DOMINO'
+//   })
+//   .then((data) => {
+//     console.log("Send SMS Data", data);
+//   })
+//   .catch((err) => {
+//     console.error("Send SMS Error", err);
+//   });
+
+const smsStatusParams = {
+  id: 'dc2bac6d-f375-4e19-9a02-ef0148991635', // 'be43b1a3-c424-4c68-8ba9-cb09266a0b03', // 
+  extended: true
+};
+greenSms.sms.fetchStatus(smsStatusParams)
+.then((data) => {
+  console.log("Send SMS Data", data);
+})
+.catch((err) => {
+  console.error("Send SMS Error", err);
+});
 
 
 
