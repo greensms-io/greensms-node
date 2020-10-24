a91f2a34c6c157d05ebfd95569bdb3ad87932282
Manan Jadhav <manan.jadhav@gmail.com>
2020-10-25 00:33:55 +0530

    Added general apis test

---
+++ test/general.test.js
@@ -0,0 +1,29 @@
+const { greenSmsInstance } = require('./../examples/greensms');
+const chai = require('chai');
+const chaiAsPromise = require('chai-as-promised');
+
+const { expect } = chai;
+
+chai.use(chaiAsPromise);
+
+describe('Additional', function() {
+
+  it('should have a key def', async function() {
+    const data = await greenSmsInstance.whois.lookup({
+      to: '919987409698',
+
+    });
+    expect(data).to.have.property('def');
+    expect(data).to.have.property('region');
+
+    // TODO: Add mandatory keys here
+  });
+
+  it('should have a key status', async function() {
+
+    const data = await greenSmsInstance.general.status();
+    expect(data).to.have.property('status');
+  });
+
+});
+
