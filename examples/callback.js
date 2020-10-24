5206ceefe02ece793c6c42ba608dd0a29390859b
Manan Jadhav <manan.jadhav@gmail.com>
2020-10-25 02:00:17 +0530

    Added callback example

---
+++ examples/callback.js
@@ -0,0 +1,8 @@
+const { greenSmsInstance } = require("./greensms");
+
+function cb(err, response) {
+  console.error('Error ', err);
+  console.log('Response', response);
+}
+
+greenSmsInstance.account.balance(cb);
