2eba55f0fd2672d90ee655543d9fc450a7cd4d57
Manan Jadhav <manan.jadhav@gmail.com>
2020-10-25 01:40:25 +0530

    Added failure test to token

--- test/token.test.js
+++ test/token.test.js
@@ -8,11 +8,22 @@ const GreenSMS = require("../dist/index.cjs.js");
 // TODO: Considering that token expire, does it make sense to use Token Requests for Tests in Deployment Pipeline
 
 // Will pick token from process.env.GREENSMS_TOKEN
-const tokenInstance = new GreenSMS({}); //
+
 
 describe('Token', function() {
   it('should have a key balance', async function() {
-    const data = await tokenInstance.account.balance();
-    expect(data).to.have.property('balance');
+    if(process.env.GREENSMS_TOKEN) {
+
+      const tokenInstance = new GreenSMS({}); //
+      const data = await tokenInstance.account.balance();
+      expect(data).to.have.property('balance');
+    }
+  });
+
+  it('should throw error if token not available', async function() {
+    process.env.GREENSMS_TOKEN = undefined;
+    if(!process.env.GREENSMS_TOKEN) {
+      await expect(tokenInstance.account.balance()).to.throw(Error);
+    }
   });
 });
