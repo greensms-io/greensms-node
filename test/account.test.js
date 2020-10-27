8151495b463f7a68b527e1cb52536d6afefcfb23
Manan Jadhav <manan.jadhav@gmail.com>
2020-10-28 01:39:18 +0530

    Added two more test cases for error

--- test/account.test.js
+++ test/account.test.js
@@ -1,5 +1,11 @@
 const { GreenSMS, greenSmsInstance } = require('./greensms');
-const { expect } = require('chai');
+
+const chai = require('chai');
+const chaiAsPromise = require('chai-as-promised');
+const { expect } = chai;
+chai.use(chaiAsPromise);
+
+
 
 describe('Account', function() {
 
@@ -23,6 +29,31 @@ describe('Account', function() {
     const data = await greenSmsInstance.account.tariff();
     expect(data).to.have.property('tariff').not.to.be.undefined;
     expect(data.tariff.length).to.greaterThan(0);
+  });
+
+  it('should throw error for Unauthorized User', async function() {
+
+    const client = new GreenSMS({
+      user: 'test_block_user', // Should come from ENB of Pipeline
+      pass: '1834561'
+    });
+
+    await expect(client.account.balance()).to.eventually.be.rejectedWith("Authorization declined");
+
+  });
+
+  it('should throw an error for Insufficient Funds', async function() {
+
+    const client = new GreenSMS({
+      user: 'test_block_user', // Should come from ENB of Pipeline
+      pass: '183456'
+    });
+
+    await expect(client.sms.send({
+      to: '919987409698',
+      txt: 'Test Message'
+    })).to.eventually.be.rejectedWith("Insufficient funds");
+
   });
 });
 
